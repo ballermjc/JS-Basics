ff2472c6750c06dea2f7c9611b607bd2aa34eee5
Michael J Cooper <ballermjc@gmail.com>
2017-11-30 19:23:58 -0700

    jsbasics all tests passed

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -5,7 +5,7 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-
+let isTyler = (name) => name == 'Tyler' ? true : false;
 //////////////////PROBLEM 2////////////////////
 
 
@@ -13,7 +13,10 @@ var name = 'Tyler';
 
 
   //Code Here
-
+var getName = function() {
+  var nameTwo = prompt("What is your name?");
+  return nameTwo;
+};
 
 //////////////////PROBLEM 3////////////////////
 
@@ -23,7 +26,10 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
+let welcome = () => {
+  var nameTwo = getName();
+  alert(`Welcome, ${nameTwo}`);
+}
 
 //////////////////PROBLEM 4////////////////////
 
@@ -33,7 +39,7 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
-
+//Parameters are the placeholders for items that will be passed into the function. Arguments are the actual items passed into the function.
 
 //////////////////PROBLEM 5////////////////////
 
@@ -43,7 +49,8 @@ var name = 'Tyler';
 
 
   //Answer Here
-
+//False, 0, null, NaN are the ones I though of. MDN also points out that undefined, '', and "". You can check if something is falsy many ways.
+//One way would be let falsyChecker = (item) => !item ? "Falsy" : "Truthy";
 
 
 //////////////////PROBLEM 6////////////////////
@@ -53,15 +60,15 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+  let myName = () => "Michael";
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-
+  let newMyName = () => myName();
 //Now alert the result of invoking newMyName
-
+alert(newMyName());
 
 
 //////////////////PROBLEM 7////////////////////
@@ -71,9 +78,14 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-
+  let outerFn = () => {
+    return function() {
+      return "Michael";
+    }
+  }
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+  let innerFn = outerFn();
 //Now invoke innerFn.
+  innerFn();
